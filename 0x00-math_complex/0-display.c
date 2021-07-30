c2391b308b48d7f8ec2dfffedbfce74497660b68
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-30 22:12:40 +0100

    Function that displays the complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,8 +1,33 @@
+#include "holberton.h"
 #include <stdio.h>
+
+/**
+ * display_complex_number - display the complex numbers
+ * @c: structure of a complex number
+ * Return: void
+ */
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-	printf("%0.f\n", c.re);
+	if (c.re == 0)
+	{
+		if (c.im < 0)
+			printf("- %.9gi\n", c.im * (-1));
+		else if (c.im == 0)
+			printf("0\n");
+		else
+			printf("%.9g\n", c.im);
+	}
 	else
-	printf("%0.f + %0.f i\n", c.re, c.im);
+	{
+		if (c.im < -1)
+			printf("%.9g - %.9gi\n", c.re, c.im * (-1));
+		else if (c.im == -1)
+			printf("%.9g - i\n", c.re);
+		else if (c.im == 0)
+			printf("%.9g\n", c.re);
+		else if (c.im == 1)
+			printf("%.9g + i\n", c.re);
+		else
+			printf("%.9g + %.9gi\n", c.re, c.im);
+	}
 }
